c20da291523a423a471f10a8d1fd0f66c077de19
wahiba9 <63967872+wahiba9@users.noreply.github.com>
2020-04-24 20:37:55 +0500

    Add files via upload

---
+++ CAFFINE program.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int no_of_milligrams,cola,coffee;
+	
+	cout<<"NUMBER OF MILLIGRAMS OF CAFFEINE=";
+	cin>>no_of_milligrams;
+	
+	
+	cola=10000/34;
+	cout<<cola<<"drinks of cola can kill a person"<<endl;	
+	coffee=10000/160;
+	cout<<coffee<<"drinks of coffee can kill a person";
+	
+	return (0);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
