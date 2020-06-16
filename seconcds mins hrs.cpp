a5d260c62c4daad7b7f5bf8284d07cceae1c1747
wahiba9 <63967872+wahiba9@users.noreply.github.com>
2020-06-16 03:02:07 -0700

    Add files via upload

---
+++ seconcds mins hrs.cpp
@@ -0,0 +1,25 @@
+//calculate sec min hours
+#include<iostream>
+using namespace std;
+int main()
+{
+	int H,M,MS,RM,S,s,RS;
+	 
+	 cout<<"ENTER SEC=";
+	 cin>>S;
+	 cout<<endl;
+	 
+
+	 H=S/3600;
+	 cout<<"HOURS="<<H<<endl;
+	 M=H*3600;
+	 RM=S-M;
+	 MS=RM/60;
+	 cout<<"MINUTES="<<MS<<endl;
+	s=MS*60;
+	RS=RM-s;
+		 cout<<"SECONDS= ="<<RS<<endl;
+	
+	  
+	 return(0);
+}
